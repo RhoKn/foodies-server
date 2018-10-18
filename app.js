2f13bf2b9c458fdecbd93db4daf28ba80117f3e9
Ruben Gonzalez <rubengonzalez@MacBook-Pro-de-Ruben.local>
2018-10-18 17:05:55 -0600

    Adición para ignorar el error de favcon

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 var app = express();
-
+function ignoreFavicon(req, res, next) {
+    if (req.originalUrl === '/favicon.ico') {
+      res.status(204).json({nope: true});
+    } else {
+      next();
+    }
+  }
 //rutas
 
 //middleware
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(ignoreFavicon);
 //cors
 
 //rutas
